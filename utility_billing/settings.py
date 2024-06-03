b6cb0822bce934fcff1a306388b683124b3e1afe
mrGerber91 <lalkakapalka3@gmail.com>
2024-06-03 13:43:04 +0300

    Update settings.py

--- utility_billing/settings.py
+++ utility_billing/settings.py
@@ -46,7 +46,7 @@ CSP_DEFAULT_SRC = ("'self'", 'https://mrgerber91-utility-billing-690d.twc1.net')
 CSP_STYLE_SRC = ("'self'", 'https://fonts.googleapis.com', 'https://cdnjs.cloudflare.com',
                  "'sha256-bviLPwiqrYk7TOtr5i2eb7I5exfGcGEvVuxmITyg//c='")
 CSP_FONT_SRC = ("'self'", 'https://fonts.gstatic.com')
-CSP_SCRIPT_SRC = ("'self'", "'unsafe-inline'")
+CSP_SCRIPT_SRC = ("'self'", "'unsafe-inline'", "https://use.fontawesome.com")
 
 ROOT_URLCONF = 'utility_billing.urls'
 
