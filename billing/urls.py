3cf3e68d39289bb9066219f1e06124343f921a2f
mrGerber91 <lalkakapalka3@gmail.com>
2024-06-03 17:17:54 +0300

    Update urls.py

--- billing/urls.py
+++ billing/urls.py
@@ -14,6 +14,9 @@ urlpatterns = [
     path('setup_rates/', views.setup_rates, name='setup_rates'),
     path('input_previous_usage/', views.input_previous_usage, name='input_previous_usage'),
     path('calculate_bill/', views.calculate_bill, name='calculate_bill'),
+
+    path('show_bill/', views.show_bill, name='show_bill'),
+
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
